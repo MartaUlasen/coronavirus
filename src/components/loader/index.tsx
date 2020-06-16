5a32a3ca0c36ebf78f203a454f30aa182c63a50d
MartaUlasen <marta.ulasen@gmail.com>
2020-06-16 17:18:53 +0300

    Create new Loader component

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { Loader } from 'react-feather';
+
+const LoaderElem: FC = () => (
+    <Loader size={20} stroke='#fff' />
+);
+
+export default LoaderElem;
